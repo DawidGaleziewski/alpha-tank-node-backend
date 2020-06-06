89bf8f95ad237402f30976eed9737fdfe0876339
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2020-06-06 16:22:29 +0200

    update: added basic server structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-const test = "bas";
+const express = require("express");
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const app = express();
+const router = new express.Router();
+
+const port = process.env.PORT || 3000;
+
+// Register
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
