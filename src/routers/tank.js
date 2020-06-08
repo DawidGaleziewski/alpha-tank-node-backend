0cf4bdf62e6cd7a4e7c1bf04594b3da3e0840396
Dawid Gałęziewski <Dawid.Galeziewski@empik.com>
2020-06-08 16:23:10 +0200

    update: added read tanks route

--- src/routers/tank.js
+++ src/routers/tank.js
@@ -14,7 +14,39 @@ router.post("/tanks", auth, async (req, res) => {
     await tank.save();
     res.status(200).send(tank);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+// Read tank
+router.get("/tanks/:id", auth, async (req, res) => {
+  try {
+    const tank = await Tank.findOne({
+      owner: req.user._id,
+      _id: req.params.id,
+    });
+
+    if (!tank) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(tank);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+// Read all tanks
+router.get("/tanks", auth, async (req, res) => {
+  try {
+    const tanks = await Tank.find({ owner: req.user._id });
+    // if (!tanks) {
+    //   return res.status(204).send(tanks);
+    // }
+
+    res.status(200).send(tanks);
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
