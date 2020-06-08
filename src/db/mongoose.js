7b2eb8e46f7f680fc53b750babf2bbff514b1db8
Dawid Gałęziewski <Dawid.Galeziewski@empik.com>
2020-06-08 11:58:16 +0200

    added atlasin connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/alpha-tank", {
+// const localURL = "mongodb://127.0.0.1:27017/alpha-tank";
+const dbName = "alpha-tank-testing-db";
+const username = "admin";
+const password = "6H7vmMaCXEcDRZoC";
+const atlasinDBurl = `mongodb+srv://${username}:${password}@kamui-cgukf.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(atlasinDBurl, {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
