daf80a459ea97c0d82fddf9e99f557b6cd035972
Dawid Gałęziewski <Dawid.Galeziewski@empik.com>
2020-06-09 12:03:22 +0200

    security: changed the way secure data is stored

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
+const secureKeys = require("../config/secureKeys");
+const { username, password, dbName } = secureKeys;
 
 // const localURL = "mongodb://127.0.0.1:27017/alpha-tank";
-const dbName = "alpha-tank-testing-db";
-const username = "admin";
-const password = "6H7vmMaCXEcDRZoC";
+
 const atlasinDBurl = `mongodb+srv://${username}:${password}@kamui-cgukf.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 mongoose.connect(atlasinDBurl, {
