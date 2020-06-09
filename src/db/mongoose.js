bcddf0876abd3e70a0b82350106dd9b74af24ab2
Dawid Gałęziewski <Dawid.Galeziewski@empik.com>
2020-06-09 12:08:26 +0200

    security: updated passwords for database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const secureKeys = require("../config/secureKeys");
-const { username, password, dbName } = secureKeys;
+const {
+  db: { username, password, dbName },
+} = secureKeys;
 
 // const localURL = "mongodb://127.0.0.1:27017/alpha-tank";
 
