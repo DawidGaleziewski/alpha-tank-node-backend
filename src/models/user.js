e4dfebad2e4edafd7a66859eb01a4c7c615feb42
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2020-06-11 12:56:52 +0200

    update: removed sensitive data returning from user

--- src/models/user.js
+++ src/models/user.js
@@ -67,6 +67,15 @@ userSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 userSchema.methods.generateToken = async function () {
   const user = this;
   const secret = "you_are_breathtaking";
